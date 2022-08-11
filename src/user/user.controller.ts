758fd4b253aeeeb1d71dec35f1700552c4747eef
donataskartanovicius <donatas.karta@gmail.com>
2022-08-11 20:20:10 +0300

    feat: create user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Put,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { CurrentUser } from './user.decorator';
+import { UserDto } from './user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get('profile')
+  @Auth()
+  async getProfile(@CurrentUser('id') id: number) {
+    return this.userService.byId(id);
+  }
+
+  @Get('by-id/:id')
+  @Auth()
+  async getUser(@Param('id') id: string) {
+    return this.userService.byId(+id);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Put(':id')
+  @Auth()
+  async updateUser(@Param('id') id: string, @Body() dto: UserDto) {
+    return this.userService.userUpdate(+id, dto);
+  }
+
+  @Get('')
+  async getUsers() {
+    return this.userService.getAll();
+  }
 }
