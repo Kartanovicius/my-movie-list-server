9a618a27fcdac71a49b2b21683e663ea176de9bb
donataskartanovicius <donatas.karta@gmail.com>
2022-08-26 21:53:56 +0300

    refactor: extend movie and tvseries with show

--- src/show/show.controller.ts
+++ src/show/show.controller.ts
@@ -1,4 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ShowDto } from './show.dto';
 import { ShowService } from './show.service';
 
 @Controller('show')
@@ -10,5 +23,57 @@ export class ShowController {
   async getShows() {
     return this.showService.getAll();
   }
+
+  @Get('most-viewed')
+  async getMostViewed() {
+    return this.showService.getMostViewedShow();
+  }
+
+  @Get('highest-rated')
+  async getHighestRated() {
+    return this.showService.getHighestRatedShow();
+  }
+
+  @Get(':id')
+  async getShowById(@Param('id') id: string) {
+    return this.showService.byId(+id);
+  }
+
   /* End Get */
+
+  /* Post */
+  @HttpCode(200)
+  @Post()
+  @Auth()
+  async createShow() {
+    return this.showService.create();
+  }
+  /* End Post */
+
+  /* Put */
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Put(':id')
+  @Auth()
+  async updateShow(@Param('id') id: string, @Body() dto: ShowDto) {
+    return this.showService.update(+id, dto);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Put('update-views/:id')
+  async updateViews(@Param('id') id: string) {
+    return this.showService.updateCountViews(+id);
+  }
+  /* End Put */
+
+  /* Delete */
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Delete(':id')
+  @Auth()
+  async deleteShow(@Param('id') id: string) {
+    return this.showService.delete(+id);
+  }
+  /* End Delete */
 }
